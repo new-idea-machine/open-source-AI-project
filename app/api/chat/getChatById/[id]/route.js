2bcb5101e1482e9bfd17f9ea55ef818bebc6df2c
Siri <sirilakku01@gmail.com>
2023-07-14 16:15:40 -0600

    Changed chatById to getChatById and move inside chat folder

---
+++ app/api/chat/getChatById/[id]/route.js
@@ -0,0 +1,23 @@
+import { connectToDB } from "@/utils/database";
+import { NextResponse } from "next/server";
+import { Conversation } from "@/models/chat";
+import { getServerSession } from "next-auth/next";
+import User from "@/models/user";
+
+export async function GET(req, context) {
+  let id = context.params.id;
+  // console.log("id", id);
+
+  try {
+    await connectToDB();
+
+    // Try to find an existing conversation with the same filename
+    let conversation = await Conversation.findById({ _id: id });
+
+    //  console.log("conversation", conversation)
+    return NextResponse.json(conversation);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json("Error fetching conversations");
+  }
+}
